1a911d1b61aaa0e71554c530a597188d9df0a70b
Broderick Hudson <broderick.hudson@mnsu.edu>
2017-10-04 19:14:46 +0000

    done!

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -11,7 +11,30 @@ class MoviesController < ApplicationController
   end
 
   def index
-    @movies = Movie.all
+    @all_ratings = Movie.all_ratings
+    
+    if (params[:ratings] == nil)
+      if (session[:ratings] == nil)
+        params[:ratings] = @all_ratings
+      else
+        params[:ratings] = session[:ratings]
+      end
+    else
+      session[:ratings] = params[:ratings]
+    end
+    
+    if (params[:sort] == nil)
+      if (session[:sort] != nil)
+        params[:sort] = session[:sort]
+      end
+    else
+      session[:sort] = params[:sort]
+    end
+    
+    @ratings = params[:ratings].keys
+    
+    @movies = Movie.where(["rating IN (?)",@ratings]).order(params[:sort])
+    #@movies = Movie.find(:all, :conditions => {:rating => (@ratings==[]?all_ratings : params[:ratings])}, :order => params[:sort])
   end
 
   def new
@@ -21,6 +44,7 @@ class MoviesController < ApplicationController
   def create
     @movie = Movie.create!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully created."
+    flash.keep
     redirect_to movies_path
   end
 
@@ -32,6 +56,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
     @movie.update_attributes!(movie_params)
     flash[:notice] = "#{@movie.title} was successfully updated."
+    flash.keep
     redirect_to movie_path(@movie)
   end
 
@@ -39,6 +64,7 @@ class MoviesController < ApplicationController
     @movie = Movie.find(params[:id])
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
+    flash.keep
     redirect_to movies_path
   end
 
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,5 @@
 class Movie < ActiveRecord::Base
+    def self.all_ratings
+        return {"G" => "G", "PG" => "PG","PG-13" => "PG-13","R" => "R","NC-17" => "NC-17"}
+    end
 end
